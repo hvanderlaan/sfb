58e358a9a8adedb77078197c3dbf067c07407f60
Harald van der Laan <harald@familievdlaan.nl>
2022-09-27 12:03:56 +0200

    Make python3 files flake8 compliant

--- api/estofex.py
+++ api/estofex.py
@@ -1,39 +1,39 @@
 ###############################################################################
-## File     : api/estofex.py                                                 ##
-## Purpose  : Send a telegram message to a telegram bot with storm forcasts  ##
-##            warnings of the European continent.                            ##
-##                                                                           ##
-## Author   : Harald van der Laan                                            ##
-## Date     : 2022/09/15                                                     ##
-## Version  : v0.0.1                                                         ##
+# File     : api/estofex.py                                                   #
+# Purpose  : Send a telegram message to a telegram bot with storm forcasts    #
+#            warnings of the European continent.                              #
+#                                                                             #
+# Author   : Harald van der Laan                                              #
+# Date     : 2022/09/15                                                       #
+# Version  : v0.0.1                                                           #
 ###############################################################################
-## Requirements:                                                             ##
-##  - Python3                                                                ##
-##  - Telegram bot from @BotFather                                           ##
+# Requirements:                                                               #
+#  - Python3                                                                  #
+#  - Telegram bot from @BotFather                                             #
 ###############################################################################
-## Changelog:                                                                ##
-##  - v0.0.1    : initial version                                            ##
+# Changelog:                                                                  #
+#  - v0.0.1    : initial version                                              #
 ###############################################################################
 
 import re
 import json
 import bs4
 import httpx
-import colorama
+
 
 def get_warning():
-    
+    """ get_warning: Function for getting the estofex warnings """
     baseurl = 'https://www.estofex.org'
     url = f'{baseurl}/cgi-bin/polygon/showforecast.cgi?listvalid=yes'
 
     with httpx.Client() as client:
         html = client.get(url)
-    
+
     soup = bs4.BeautifulSoup(html.text, 'html.parser')
-    
+
     try:
         level = soup.tr.findNextSiblings()[1].img.findNext()
-        
+
         if re.match('.*1.png', str(level)):
             forecastlevel = 1
         elif re.match('.*2.png', str(level)):
@@ -42,7 +42,7 @@ def get_warning():
             forecastlevel = 3
         else:
             forecastlevel = None
-    
+
         forecastlink = soup.a['href']
         forecastimage = soup.img['src']
 
@@ -60,7 +60,7 @@ def get_warning():
 def get_forecast(url):
     with httpx.Client() as client:
         html = client.get(url)
-    
+
     soup = bs4.BeautifulSoup(html.text, 'html.parser')
     forecast = re.sub('<br/>', '\n', str(soup.p.findNextSiblings()[1]))
     forecast = re.sub('<[^>]+>', '', str(forecast))
@@ -68,6 +68,6 @@ def get_forecast(url):
     result = ''
     for line in forecast.splitlines():
         if re.match('A level', str(line)):
-            result   += '*' + line + '*' + '\n\n'
+            result += '*' + line + '*' + '\n\n'
 
     return result
